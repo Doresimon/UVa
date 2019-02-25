067467a39741855784ecb4acdf012c9c21dcc5f6
Doresimon <doresimon@163.com>
2019-02-25 00:06:46 -0800

    .

--- UVa455/main.cpp
+++ UVa455/main.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
-#include <cstdio>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
 
-    freopen("input", "r", stdin);
+    // freopen("input", "r", stdin);
 
     int N;
-    char[80] str;
+    char str[80];
+    bool period[80];
     cin >> N;
-    cout << N << endl;
 
-    while (N > 0)
-    { /* input */
+    char *p;
+    int j;
+    bool ok, all;
 
+    while (true)
+    {
+        // clear string
+        memset(str, 0, 80);
         cin >> str;
-        cout << N << str;
-
+        all = true;
+        int len = strlen(str);
+        for (int i = 1; i < len; i++)
+        {
+            if (len % i != 0)
+            {
+                continue;
+            }
+            ok = true;
+            p = &str[i];
+            j = 0;
+            while (*p)
+            {
+                if (*p != str[j])
+                {
+                    ok = false;
+                    break;
+                }
+                p++;
+                j++;
+                j = j == i ? 0 : j;
+            }
+            if (ok)
+            {
+                all = false;
+                cout << i << endl;
+                break;
+            }
+        }
         N--;
+        if (all)
+        {
+            cout << len << endl;
+        }
+        if (N > 0)
+        {
+            cout << endl;
+        }
+        else
+        {
+            break;
+        }
     }
 
     return 0;
